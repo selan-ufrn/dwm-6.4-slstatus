2ec8fb0e40adf822a121f5fe3de55c182bb34c96
Selan R dos Santos <selan.santos@ufrn.br>
2023-07-09 16:15:25 -0300

    added color files

---
+++ colors/stock.h
@@ -0,0 +1,13 @@
+static const char sel_fg[]          = "#ffffff";
+static const char sel_bg[]          = "#000000";
+static const char norm_fg[]         = "#777777";
+static const char norm_bg[]         = "#000000";
+
+static const char norm_border[]     = "#444444";
+static const char sel_border[]      = "#00FFFF";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm]  = { norm_fg,   norm_bg,   norm_border},
+	[SchemeSel]   = { sel_fg,    sel_bg,    sel_border },
+};
+
